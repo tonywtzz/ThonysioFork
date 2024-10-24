4eded85f3e688bf3d2994c443bda6b08be9b1756
Anthony <0001068929@senaimgaluno.com.br>
2024-10-24 15:16:27 -0300

    enviando arquivo 18

--- ConhendoFork/src/Ex18.java
+++ ConhendoFork/src/Ex18.java
@@ -1,10 +1,35 @@
 /*Cálculo de Média com Condição: Leia 5 notas de alunos e calcule a média, 
 informando se o aluno foi aprovado (média >= 7) ou reprovado.
 */
+import java.util.Scanner;
 public class Ex18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Digite a 1º nota: ");
+		double nota1 = scanner.nextDouble();
+		
+		System.out.println("Digite a 2º nota: ");
+		double nota2 = scanner.nextDouble();
+		
+		System.out.println("Digite a 3º nota: ");
+		double nota3 = scanner.nextDouble();
+		
+		System.out.println("Digite a 4º nota: ");
+		double nota4 = scanner.nextDouble();
+		
+		System.out.println("Digite a 5º nota: ");
+		double nota5 = scanner.nextDouble();
+		
+		double media = nota1 + nota2 + nota3 + nota4 + nota5;
+		
+		if (media >= 7) {
+			System.out.print("Com a nota " + media + " voce está aprovado");
+		}
+		else {
+			System.out.print("Com a nota " + media + " voce está reprovado");
+		}
 
 	}
 
